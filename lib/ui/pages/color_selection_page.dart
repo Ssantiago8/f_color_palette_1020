992ea0d391e90614f2a84ef98948831d908b9e95
Ssantiago8 <54732915+Ssantiago8@users.noreply.github.com>
2022-08-15 01:35:36 -0500

    Complete app

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -25,9 +25,43 @@ class ColorSelectionPage extends StatelessWidget {
           ],
         ),
         // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
+        body: (Align(
+          alignment: Alignment.topLeft,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ColorPalette(
+                  baseColor: 'A9E4EF',
+                  baseColor1: '1CD6CE',
+                  baseColor2: 'FEDB39',
+                  baseColor3: 'D61C4E',
+                  callback: (String) {},
+                ),
+                ColorPalette(
+                  baseColor: '7A306C',
+                  baseColor1: 'EF6351',
+                  baseColor2: 'C4CAD0',
+                  baseColor3: '878C8F',
+                  callback: (String) {},
+                ),
+                ColorPalette(
+                  baseColor: '1C1F33',
+                  baseColor1: 'FF220C',
+                  baseColor2: '9B7874',
+                  baseColor3: 'EE6C4D',
+                  callback: (String) {},
+                ),
+                ColorPalette(
+                  baseColor: '42BFDD',
+                  baseColor1: 'FF66B3',
+                  baseColor2: 'F0F6F6',
+                  baseColor3: 'F7DD72',
+                  callback: (String) {},
+                )
+              ],
+            ),
+          ),
         )));
   }
 
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -1,28 +1,79 @@
 import 'package:flutter/material.dart';
 import '../../utils/color_utils.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 class ColorPalette extends StatelessWidget {
   const ColorPalette({
     Key? key,
-    // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
+    required this.baseColor1,
+    required this.baseColor2,
+    required this.baseColor3,
+    required this.baseColor,
+    required this.callback,
   }) : super(key: key);
 
   // atributos de la clase, por ejemplo
-  //final String baseColor;
+  final String baseColor;
+  final String baseColor1;
+  final String baseColor2;
+  final String baseColor3;
 
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback;
 
   @override
   Widget build(BuildContext context) {
     // padding para crear espacio alrededor del widget
+    int color0 = int.parse('0xFF' + baseColor);
+    int color1 = int.parse('0xFF' + baseColor1);
+    int color2 = int.parse('0xFF' + baseColor2);
+    int color3 = int.parse('0xFF' + baseColor3);
     return Padding(
       padding: const EdgeInsets.all(8.0),
       // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
+      child:
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+        Expanded(
+            flex: 4,
+            child: InkWell(
+                onTap: () {
+                  showColor(baseColor);
+                },
+                child: Ink(height: 100, color: Color(color0)))),
+        Expanded(
+            flex: 3,
+            child: InkWell(
+                onTap: () {
+                  showColor(baseColor1);
+                },
+                child: Ink(height: 100, color: Color(color1)))),
+        Expanded(
+            flex: 2,
+            child: InkWell(
+                onTap: () {
+                  showColor(baseColor2);
+                },
+                child: Ink(height: 100, color: Color(color2)))),
+        Expanded(
+            flex: 1,
+            child: InkWell(
+                onTap: () {
+                  showColor(baseColor3);
+                },
+                child: Ink(height: 100, color: Color(color3)))),
+      ]),
+    );
+  }
+
+  void showColor(String value) {
+    Clipboard.setData(ClipboardData(text: value));
+    Get.snackbar(
+      'Color palette',
+      'Valor copiado = $value',
+      backgroundColor: ColorUtils.FromHex(value),
+      snackPosition: SnackPosition.BOTTOM,
     );
   }
 }
